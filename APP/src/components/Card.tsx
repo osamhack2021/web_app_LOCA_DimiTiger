57afd301eb83a95c1b088e8676b0ea4b4fbbce5e
Jay <me@wjay.kim>
2021-09-16 23:51:14 +0900

    feat(app): add card component

---
+++ APP/src/components/Card.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {colorDivider, colorWhite} from '../constants/colors';
+import {styleShadow} from '../constants/styles';
+
+type CardProps = {
+  title: string;
+  children: React.ReactChild;
+};
+
+const Card: React.FC<CardProps> = ({title, children}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>{title}</Text>
+      <View style={styles.divider} />
+      <View style={styles.contentContainer}>{children}</View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...styleShadow,
+    backgroundColor: colorWhite,
+    borderRadius: 20,
+    margin: 20,
+  },
+  titleText: {
+    fontWeight: 'bold',
+    fontSize: 21,
+    margin: 20,
+  },
+  divider: {
+    backgroundColor: colorDivider,
+    height: 1,
+    width: '100%',
+  },
+  contentContainer: {
+    margin: 20,
+  },
+});
+
+export default Card;
--- APP/src/constants/colors.ts
+++ APP/src/constants/colors.ts
@@ -1,1 +1,3 @@
 export const colorWhite = '#fff';
+export const colorBlack = '#000';
+export const colorDivider = '#e4e4e4';
