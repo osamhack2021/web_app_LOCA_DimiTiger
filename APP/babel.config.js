a92d82dd4d09a8e091162f6906ead73ed1c8c6fe
Jay <me@wjay.kim>
2021-09-16 22:56:23 +0900

    feat(app): add header component

--- APP/babel.config.js
+++ APP/babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@api': './src/api',
+          '@atoms': './src/atoms',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@images': './assets/images',
+          '@models': './src/models',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
