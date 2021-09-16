c7b6b8c10ca01e0853dabf166f14357d665fcce5
Jay <me@wjay.kim>
2021-09-16 23:51:14 +0900

    feat(app): add card component

---
+++ APP/src/constants/styles.ts
@@ -0,0 +1,16 @@
+import {Platform, ViewStyle} from 'react-native';
+import {colorBlack} from './colors';
+
+export const styleShadow: ViewStyle = {
+  ...Platform.select({
+    android: {
+      elevation: 10,
+    },
+    default: {
+      shadowColor: colorBlack,
+      shadowOffset: {width: 0, height: 8},
+      shadowOpacity: 0.25,
+      shadowRadius: 10,
+    },
+  }),
+};
--- APP/src/screens/MainScreen.tsx
+++ APP/src/screens/MainScreen.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import {View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import Card from '../components/Card';
 import Header from '../components/Header';
 
 const MainScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Card title="공지사항">
+          <View />
+        </Card>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
+});
 
 export default MainScreen;
