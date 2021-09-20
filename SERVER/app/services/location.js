5e1cfb70e3f1e7823cd79d773c5c1b3c84fba993
김지섭 <0226daniel@gmail.com>
2021-09-20 10:38:46 +0900

    feat(server): add services

---
+++ SERVER/app/services/location.js
@@ -0,0 +1,33 @@
+const User = require('../models/user');
+const Location = require('../models/location');
+const { createError } = require('../utils/error');
+
+const Errors = (exports.Errors = {
+	LocationNotFoundError: createError('LocationNotFoundError'),
+});
+
+exports.getLocations = async () => {
+	return await Location.find();
+};
+
+exports.getLocation = async (_id) => {
+	return await Location.findById(_id);
+};
+
+exports.createLocation = async ({ name, ui }) => {
+	return await new Location({ name, ui }).save();
+};
+
+exports.updateLocation = async (_id, fields) => {
+	const location = await Location.findById(_id).exec();
+
+	if (!location) throw new Errors.UserNotFoundError();
+
+	for (const key in fields) {
+		location[key] = fields[key];
+	}
+
+	await location.save();
+
+	return location;
+};
