291648b0ef61940222e10901c322d2ca0d010711
김지섭 <0226daniel@gmail.com>
2021-09-20 10:38:19 +0900

    feat(server): user apis

--- SERVER/app/controllers/user.js
+++ SERVER/app/controllers/user.js
@@ -3,12 +3,96 @@ const Joi = require('joi');
 const UserService = require('../services/user');
 
 exports.getUsers = {
-	tags: ['api', 'public-api', 'user'],
+	tags: ['api', 'user'],
 	description: '사용자 목록을 가져옵니다.',
 	validate: {},
-	handler: async (request, h) => {
+	handler: async (req, h) => {
 		try {
 			return await UserService.getUsers();
+		} catch (err) {
+			throw Boom.internal(err);
+		}
+	},
+};
+
+exports.createUsers = {
+	tags: ['api', 'user'],
+	validate: {
+		payload: Joi.object({
+			serial: Joi.string().required(),
+			name: Joi.string().required(),
+			password: Joi.string().required(),
+		}),
+	},
+	handler: async (req, h) => {
+		try {
+			return await UserService.createUsers(req.payload);
+		} catch (err) {
+			throw Boom.internal(err);
+		}
+	},
+};
+
+exports.updateUsers = {
+	auth: {
+		mode: 'optional',
+	},
+	tags: ['api', 'user'],
+	validate: {
+		query: Joi.object({
+			userId: Joi.string().description('사용자 _id'),
+		}),
+		payload: Joi.object({
+			identity: Joi.object({
+				_id: Joi.string().required(),
+			}),
+			register: Joi.object({
+				id: Joi.string(),
+				phone: Joi.string(),
+				email: Joi.string(),
+				password: Joi.string(),
+				name: Joi.string(),
+			}),
+		}),
+	},
+	handler: async (req, h) => {
+		try {
+			return await UserService.updateUsers(
+				req.payload.identity._id,
+				req.payload.register
+			);
+		} catch (err) {
+			throw Boom.internal(err);
+		}
+	},
+};
+
+exports.registerUsers = {
+	auth: {
+		mode: 'optional',
+	},
+	tags: ['api', 'user'],
+	validate: {
+		payload: Joi.object({
+			identity: Joi.object({
+				serial: Joi.string().required(),
+				name: Joi.string().required(),
+				password: Joi.string().required(),
+			}),
+			register: Joi.object({
+				id: Joi.string().required(),
+				phone: Joi.string().required(),
+				email: Joi.string().required(),
+				password: Joi.string().required(),
+			}),
+		}),
+	},
+	handler: async (req, h) => {
+		try {
+			return await UserService.registerUsers(
+				req.payload.identity,
+				req.payload.register
+			);
 		} catch (err) {
 			throw Boom.internal(err);
 		}
--- SERVER/app/services/user.js
+++ SERVER/app/services/user.js
@@ -3,10 +3,97 @@ const { createError } = require('../utils/error');
 
 const Errors = (exports.Errors = {
 	UserNotFoundError: createError('UserNotFoundError'),
+	PasswordNotMatchError: createError('PasswordNotMatchError'),
 });
 
 exports.getUsers = async () => {
 	const query = User.find();
 
 	return await query.exec();
+};
+
+exports.getUser = async ({ id }) => {
+	const query = User.find({ id });
+
+	return await query.exec();
+};
+
+exports.createUsers = async ({ serial, name, password }) => {
+	return await new User({
+		serial,
+		name,
+		password: await User.hashPassword(password),
+	}).save();
+};
+
+exports.updateUsers = async (_id, fields) => {
+	const user = await User.findOne({
+		_id,
+	}).exec();
+
+	if (!user) throw new Errors.UserNotFoundError();
+
+	for (const key in fields) {
+		if (key == 'password') user[key] = await User.hashPassword(fields[key]);
+		else user[key] = fields[key];
+	}
+
+	await user.save();
+
+	return user;
+};
+
+exports.registerUsers = async ({ serial, name, password }, fields) => {
+	const user = await User.findOne({
+		serial,
+		name,
+	})
+		.select('+password')
+		.exec();
+
+	if (!user) throw new Errors.UserNotFoundError();
+	if (!(await user.verifyPassword(password)))
+		throw new Errors.PasswordNotMatchError();
+
+	for (const key in fields) {
+		if (key == 'password') user[key] = await User.hashPassword(fields[key]);
+		else user[key] = fields[key];
+	}
+
+	await user.save();
+
+	return user;
+};
+
+exports.idLogin = async ({ id, password }) => {
+	const user = User.findOne({
+		id,
+	}).exec();
+
+	if (!(await user.verifyPassword(password)))
+		throw new Errors.PasswordNotMatchError();
+
+	return user;
+};
+
+exports.emailLogin = async ({ email, password }) => {
+	const user = User.findOne({
+		email,
+	}).exec();
+
+	if (!(await user.verifyPassword(password)))
+		throw new Errors.PasswordNotMatchError();
+
+	return user;
+};
+
+exports.serialLogin = async ({ serial, password }) => {
+	const user = User.findOne({
+		serial,
+	}).exec();
+
+	if (!(await user.verifyPassword(password)))
+		throw new Errors.PasswordNotMatchError();
+
+	return user;
 };
