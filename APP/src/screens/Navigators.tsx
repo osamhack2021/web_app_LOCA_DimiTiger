336fc56ca5a66b4bcc8411f07a67feb7c9f9e41c
Jay <me@wjay.kim>
2021-09-22 00:00:02 +0900

    feat(app): add authentication flow

--- APP/src/screens/Navigators.tsx
+++ APP/src/screens/Navigators.tsx
@@ -2,23 +2,32 @@ import { RouteProp } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StackNavigationProp, StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
+import { useAuth } from '../api/auth';
 import MainScreen from './MainScreen';
 import NoticeScreen from './NoticeScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const RootStack = () => {
+  const { authorized } = useAuth({});
   return (
     <Stack.Navigator
       initialRouteName="MainScreen"
       screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="MainScreen" component={MainScreen} />
-      <Stack.Screen name="NoticeScreen" component={NoticeScreen} />
+      {authorized ? (
+        <>
+          <Stack.Screen name="MainScreen" component={MainScreen} />
+          <Stack.Screen name="NoticeScreen" component={NoticeScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="AuthScreen" component={MainScreen} />
+      )}
     </Stack.Navigator>
   );
 };
 
 export type RootStackParamList = {
+  AuthScreen: undefined;
   MainScreen: undefined;
   NoticeScreen: undefined;
 };
