abc43ba7e1f6fdb3ca48934b61bce88085d9b6b9
Jay <me@wjay.kim>
2021-09-24 00:01:26 +0900

    revert(app): remove reanimated module

--- APP/android/app/src/main/java/io/dimitiger/loca/MainApplication.java
+++ APP/android/app/src/main/java/io/dimitiger/loca/MainApplication.java
@@ -8,9 +8,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.soloader.SoLoader;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -36,11 +34,6 @@ public class MainApplication extends Application implements ReactApplication {
                 @Override
                 protected String getJSMainModuleName() {
                     return "index";
-                }
-
-                @Override
-                protected JSIModulePackage getJSIModulePackage() {
-                    return new ReanimatedJSIModulePackage(); // <- add
                 }
             };
 
