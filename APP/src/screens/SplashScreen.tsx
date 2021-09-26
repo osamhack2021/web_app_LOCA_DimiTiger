cb5f2c8bddf440888e6adedce011a882f70ad1e9
Jay <me@wjay.kim>
2021-09-26 18:56:33 +0900

    feat(app): add splash screen

--- APP/src/screens/SplashScreen.tsx
+++ APP/src/screens/SplashScreen.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import Logo from '@images/loca_logo.svg';
+
+import { colorSplashBg } from '@/constants/colors';
 
 const SplashScreen = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Logo />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: colorSplashBg,
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export default SplashScreen;
