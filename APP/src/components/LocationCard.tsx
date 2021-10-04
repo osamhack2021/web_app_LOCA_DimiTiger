228cdf9708e05260998920c9ca9b9d79eb83ef9d
Jay <me@wjay.kim>
2021-10-04 22:34:41 +0900

    fix(app): fix location chip not clicking

--- APP/src/components/LocationCard.tsx
+++ APP/src/components/LocationCard.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Animated, {
   FadeIn,
   FadeInUp,
