5e2c43e96861eaf4ac82fc822fbb40bb51fc6e80
Jay <me@wjay.kim>
2021-10-06 17:45:18 +0900

    feat(app): change noti message

--- APP/src/utils/backgroundMontorHandler.ts
+++ APP/src/utils/backgroundMontorHandler.ts
@@ -16,12 +16,12 @@ export default async function ({ identifier, state }: BackgroundMonitorEvent) {
   const isEnter = state === 'inside';
   const channelId = await notifee.createChannel({
     id: isEnter ? 'enter' : 'exit',
-    name: `LOCA 위치 ${isEnter ? '입' : '퇴'}장 알림`,
+    name: `LOCA 위치 ${isEnter ? '진입' : '이탈'} 알림`,
   });
 
   await notifee.displayNotification({
-    title: '위치 출입 탐지',
-    body: '위치 출입이 탐지되었습니다.',
+    title: '위치 탐지 알림',
+    body: `${beacon.location.name}에${isEnter ? ' 진입' : '서 이탈'}했습니다.`,
     android: {
       channelId,
     },
