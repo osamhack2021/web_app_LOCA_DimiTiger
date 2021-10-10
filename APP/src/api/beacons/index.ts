daef2f46b696003a99a92b04b2e4b4298191ffda
Jay <me@wjay.kim>
2021-10-10 18:30:06 +0900

    fix(app): fix auth not persisted

--- APP/src/api/beacons/index.ts
+++ APP/src/api/beacons/index.ts
@@ -1,10 +1,16 @@
+import { useRecoilValueLoadable } from 'recoil';
+
+import { accessTokenState } from '@/atoms';
 import usePaginationQuery from '@/hooks/usePaginationQuery';
 import Beacon from '@/types/Beacon';
 import PaginationQuery from '@/types/PaginationQuery';
 
 export function useBeacons() {
+  const { state, contents } = useRecoilValueLoadable(accessTokenState);
   const query: PaginationQuery = {
     limit: 0,
   };
-  return usePaginationQuery<Beacon>('/beacons', query);
+  return usePaginationQuery<Beacon>('/beacons', query, {
+    enabled: state === 'hasValue' && !!contents,
+  });
 }
