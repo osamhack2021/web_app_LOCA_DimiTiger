96e7982f9a62d2795bb482f640f2e65bab05f2ae
박재성 <jspark6815@daum.net>
2021-10-10 06:56:28 +0000

    fix(web): fix sidebar animation

--- WEB/src/components/Sidebar/Sidebar.tsx
+++ WEB/src/components/Sidebar/Sidebar.tsx
@@ -15,11 +15,9 @@ interface IProps {
 const SidebarComponent = (props: IProps) => {
   return (
     <Link to={props.link}>
-      <div className={`sidebar_item${props.hover && ' flexStart'}`}>
+      <div className={`sidebar_item ${props.hover && ' flexStart'}`}>
         <img src={props.imgSrc} alt="" />
-        {props.hover && (
-          <div className="sidebar_item_name">{props.eleName}</div>
-        )}
+        <div className="sidebar_item_name">{props.eleName}</div>
       </div>
     </Link>
   );
@@ -33,30 +31,29 @@ const Sidebar = () => {
       imgSrc: './icons/sidebar_ico_home.svg',
       eleName: '홈',
       link: '/',
-    },
-    {
+    }, {
       imgSrc: './icons/sidebar_ico_search.svg',
       eleName: '유동병력검색',
       link: '/search',
-    },
-    {
+    }, {
       imgSrc: './icons/sidebar_ico_add_user.svg',
       eleName: '인원현황',
-      link: '/',
-    },
-    {
+      link: '/users',
+    }, {
       imgSrc: './icons/sidebar_ico_users.svg',
       eleName: '전체인원',
       link: '/',
-    },
-    {
+    }, {
       imgSrc: './icons/sidebar_ico_worker.svg',
       eleName: '근무자등록',
       link: '/',
-    },
-    {
+    }, {
       imgSrc: './icons/sidebar_ico_setting.svg',
       eleName: '설정',
+      link: '/',
+    }, {
+      imgSrc: '/icons/sidebar_ico_logout.svg',
+      eleName: '로그아웃',
       link: '/',
     },
   ];
@@ -77,13 +74,6 @@ const Sidebar = () => {
           />
         );
       })}
-      <div
-        id="logout"
-        className={`sidebar_item${hover && ' flexStart'}`}
-        onClick={logout}>
-        <img src="/icons/sidebar_ico_logout.svg" alt="" />
-        {hover && <div className="sidebar_item_name">로그아웃</div>}
-      </div>
     </div>
   );
 };
