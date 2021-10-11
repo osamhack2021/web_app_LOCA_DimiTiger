bbcbab545d5649f06ba206f13187de192b34cd7a
박재성 <jspark6815@daum.net>
2021-10-11 02:21:02 +0000

    feat(web): add users page modal

--- WEB/src/components/LayoutContentWrapper.tsx
+++ WEB/src/components/LayoutContentWrapper.tsx
@@ -4,6 +4,7 @@ const LayoutContentWrapper = styled.div`
   background-color: #f2f3f5;
   display: flex;
   flex-direction: column;
+  min-height: 100vh;
 `;
 
 export default LayoutContentWrapper;
--- WEB/src/pages/Users/index.tsx
+++ WEB/src/pages/Users/index.tsx
@@ -1,80 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Button, Form, Input, Table } from 'antd';
+import { Form, Input, Modal, Table } from 'antd';
 import styled from 'styled-components';
-import {
-  NumberParam,
-  StringParam,
-  useQueryParams,
-} from 'use-query-params';
+import { NumberParam, StringParam, useQueryParams } from 'use-query-params';
 
+import { useDeleteUser, useUsers } from '../../api/users';
+import AddButton from '../../components/AddButton';
+import DeleteButton from '../../components/DeleteButton';
 import Header from '../../components/Header/Header';
 import LargeCard from '../../components/LargeCard';
 import LayoutContent from '../../components/LayoutContent';
 import LayoutContentWrapper from '../../components/LayoutContentWrapper';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import User from '../../types/User';
-import { useUsers } from '../../api/users';
-import AddButton from '../../components/AddButton';
 
 const { Search } = Input;
-
-const columns = [
-  {
-    title: '군번',
-    dataIndex: 'serial',
-    key: 'serial',
-    width: '20%',
-    render: (serial: string) =>
-      <>{serial}</>
-  }, {
-    title: '이름',
-    dataIndex: 'name',
-    key: 'name',
-    width: '10%',
-    render: (name: User["name"]) => (
-      <>{name}</>
-    ),
-  }, {
-    title: '계급',
-    dataIndex: 'rank',
-    key: 'rank',
-    width: '10%',
-    render: (rank: User["rank"]) => (
-      <>{rank}</>
-    ),
-  }, {
-    title: '이메일',
-    dataIndex: 'email',
-    key: 'email',
-    render: (email: User["email"]) => (
-      <>{email}</>
-    ),
-  }, {
-    title: '전화번호',
-    dataIndex: 'phone',
-    key: 'phone',
-    render: (phone: User["phone"]) => (
-      <>{phone}</>
-    ),
-  }, {
-    title: '가입여부',
-    dataIndex: 'registered',
-    key: 'registered',
-    width: '7%',
-    render: (registered: User["registered"]) => (
-      <>{registered}</>
-    ),
-  }, {
-    title: '',
-    dataIndex: '_id',
-    key: '_id',
-    width: '5%',
-    render: (_id: User["_id"]) => (
-      <>{_id}</>
-    ),
-  },
-];
 
 const Users = () => {
   const history = useHistory();
@@ -89,6 +29,77 @@ const Users = () => {
     page: query.page || undefined,
     limit: query.limit || undefined,
   });
+  const columns = [
+    {
+      title: '군번',
+      dataIndex: 'serial',
+      key: 'serial',
+      width: '20%',
+      render: (serial: string) => <>{serial}</>,
+    },
+    {
+      title: '이름',
+      dataIndex: 'name',
+      key: 'name',
+      width: '10%',
+      render: (name: User['name']) => <>{name}</>,
+    },
+    {
+      title: '계급',
+      dataIndex: 'rank',
+      key: 'rank',
+      width: '10%',
+      render: (rank: User['rank']) => <>{rank}</>,
+    },
+    {
+      title: '이메일',
+      dataIndex: 'email',
+      key: 'email',
+      render: (email: User['email']) => <>{email}</>,
+    },
+    {
+      title: '전화번호',
+      dataIndex: 'phone',
+      key: 'phone',
+      render: (phone: User['phone']) => <>{phone}</>,
+    },
+    {
+      title: '가입여부',
+      dataIndex: 'registered',
+      key: 'registered',
+      width: '7%',
+      render: (registered: User['registered']) => <>{registered}</>,
+    },
+    {
+      title: '',
+      dataIndex: '_id',
+      key: '_id',
+      width: '5%',
+      render: (_id: User['_id']) => (
+        <DeleteButton onClick={deleteUser} name={_id}></DeleteButton>
+      ),
+    },
+  ];
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const deleteUserMutation = useDeleteUser();
+
+  const deleteUser = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const button: HTMLButtonElement = event.currentTarget;
+    const _id: User['_id'] = button.name;
+    deleteUserMutation.mutate({ _id });
+  };
 
   return (
     <LayoutContentWrapper>
@@ -111,7 +122,16 @@ const Users = () => {
                   );
                 }}
                 layout="inline">
-                <AddButton> </AddButton>
+                <AddButton onClick={showModal}></AddButton>
+                <Modal
+                  title="인원 추가"
+                  centered
+                  visible={isModalVisible}
+                  onOk={handleOk}
+                  okText="추가"
+                  onCancel={handleCancel}>
+                  <Input></Input>
+                </Modal>
               </Form>
             </ToolkitWrap>
           }>
