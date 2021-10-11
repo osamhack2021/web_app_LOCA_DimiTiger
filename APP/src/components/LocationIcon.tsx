637e28f57c8efe7509bc019e0a70132cc7197287
Jay <me@wjay.kim>
2021-10-12 01:02:19 +0900

    fix(app): fix icon size problem

--- APP/src/components/LocationIcon.tsx
+++ APP/src/components/LocationIcon.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { View } from 'react-native';
 import { SvgProps, SvgXml } from 'react-native-svg';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useQuery } from 'react-query';
 import axios from 'axios';
+
+import { colorEllipsis } from '@/constants/colors';
 
 async function getSvg(icon: string) {
   const { data } = await axios.get(
@@ -18,7 +22,22 @@ const LocationIcon = (props: LocationIconProps) => {
   const { data } = useQuery(['svg', props.icon], () => getSvg(props.icon), {
     cacheTime: Infinity,
   });
-  return <SvgXml {...props} xml={data || null} />;
+  const size = useMemo(
+    () =>
+      typeof props.height === 'string'
+        ? parseInt(props.height, 10)
+        : props.height,
+    [props.height],
+  );
+  return (
+    <View style={{ width: size, height: size }}>
+      {data ? (
+        <SvgXml {...props} xml={data || null} />
+      ) : (
+        <Icon name="cloud-sync-outline" color={colorEllipsis} size={size} />
+      )}
+    </View>
+  );
 };
 
 export default LocationIcon;
