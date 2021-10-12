687678d7689a0759a57633657d595c10e59e01d2
Jay <me@wjay.kim>
2021-10-13 08:13:38 +0900

    refactor(web): improve header

--- WEB/src/components/Header/Header.tsx
+++ WEB/src/components/Header/Header.tsx
@@ -1,82 +1,42 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
+import { ko } from 'date-fns/locale';
 
 import './Header.css';
 
-interface iprops {}
-interface istate {
-  date: Date;
-  timeID: any;
-  watch1: string;
-  watch2: string;
-  watch3: string;
-}
-class Header extends Component<iprops, istate> {
-  constructor(props: iprops) {
-    super(props);
-    this.state = {
-      date: new Date(),
-      timeID: '',
-      watch1: '',
-      watch2: '',
-      watch3: '',
-    };
-  }
+const Header = () => {
+  const [date, setDate] = useState(new Date());
 
-  componentDidMount() {
-    this.setState({
-      timeID: setInterval(() => this.Change(), 1000),
-    });
-  }
-  componentWillUnmount() {
-    clearInterval(this.state.timeID);
-  }
-  Change = () => {
-    this.setState({
-      date: new Date(),
-      watch1: this.getToday(),
-      watch2:
-        ('0' + this.state.date.getHours()).slice(-2) +
-        ':' +
-        ('0' + this.state.date.getMinutes()).slice(-2),
-      watch3: ('0' + this.state.date.getSeconds()).slice(-2),
-    });
-  };
+  useEffect(() => {
+    const interval = setInterval(() => setDate(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-  getToday = () => {
-    const week: Array<string> = ['일', '월', '화', '수', '목', '금', '토'];
-    const date: Date = this.state.date;
-    const year: string = String(date.getFullYear());
-    const month: string = ('0' + (1 + date.getMonth())).slice(-2);
-    const day: string = ('0' + date.getDate()).slice(-2);
-    const dayOfTheWeek: number = date.getDay();
-    return year + '-' + month + '-' + day + ' ' + week[dayOfTheWeek] + '요일';
-  };
-
-  render() {
-    return (
-      <header>
-        <div id="belong">
-          <Link to="/">
-            <img id="belong_logo" src="/icons/25div.png" alt="logo" />
-            <p className="belong_name">
-              70여단 <span>국사봉대대</span>
-            </p>
-          </Link>
-        </div>
-        <div id="watch_widget">
-          <div className="vertical_line"></div>
-          <div className="watch">
-            <div className="date">{this.state.watch1}</div>
-            <div className="time">
-              {this.state.watch2}
-              <div className="seconds">{this.state.watch3}</div>
-            </div>
+  return (
+    <header>
+      <div id="belong">
+        <Link to="/">
+          <img id="belong_logo" src="/icons/25div.png" alt="logo" />
+          <p className="belong_name">
+            70여단 <span>국사봉대대</span>
+          </p>
+        </Link>
+      </div>
+      <div id="watch_widget">
+        <div className="vertical_line"></div>
+        <div className="watch">
+          <div className="date">
+            {format(date, 'yyyy-MM-dd EEEE', { locale: ko })}
+          </div>
+          <div className="time">
+            {format(date, 'HH:mm')}
+            <div className="seconds">{format(date, 'ss')}</div>
           </div>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+    </header>
+  );
+};
 
-export default Header;
+export default React.memo(Header);
