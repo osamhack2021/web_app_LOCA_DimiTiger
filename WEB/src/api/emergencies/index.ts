7f6151b1c086ce766b86fd06e271f9a074b7ff66
Jay <me@wjay.kim>
2021-10-17 02:23:31 +0900

    feat(web): create emergency reports files

---
+++ WEB/src/api/emergencies/index.ts
@@ -0,0 +1,14 @@
+import usePaginationQuery from '../../hooks/usePaginationQuery';
+import useQuery from '../../hooks/useQuery';
+import EmergencyReport from '../../types/EmergencyReport';
+import EmergencyReportQuery from '../../types/EmergencyReportQuery';
+
+export function useEmergencyReports(query?: EmergencyReportQuery) {
+  return usePaginationQuery<EmergencyReport>('/emergencies', query, {
+    refetchInterval: 1000,
+  });
+}
+
+export function useEmergencyReport(id: string) {
+  return useQuery<EmergencyReport>('/emergencies', id);
+}
