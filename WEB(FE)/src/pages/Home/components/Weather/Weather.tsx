4c78db76d3f57f9a372abf7657fbf8c6515c97bb
박재성 <jspark6815@daum.net>
2021-10-18 14:09:13 +0000

    feat(web): connect weathers api

--- WEB(FE)/src/pages/Home/components/Weather/Weather.tsx
+++ WEB(FE)/src/pages/Home/components/Weather/Weather.tsx
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useRecoilValue } from 'recoil';
 
 import './Weather.css';
 
-class Weather extends Component {
-  render() {
-    return (
-      <div id="weather" className="dash_component">
-        <div id="weather_head">
-          <img src="./icons/weather.svg" alt="weatherIco" />
-          <div id="weather_num">31°</div>
-        </div>
-        <div id="weather_contents">구름조금</div>
+import { useWeathers } from '@/api/weathers';
+import { settingsState } from '@/atoms';
+
+const Weather = () => {
+  const { weather } = useRecoilValue(settingsState);
+  const weathers = useWeathers({ city: weather.location });
+  return (
+    <div id="weather" className="dash_component">
+      <div id="weather_head">
+        <img src="./icons/weather.svg" alt="weatherIco" />
+        <div id="weather_num">{Math.floor(weathers.data?.temp)}</div>
       </div>
-    );
-  }
-}
+      <div id="weather_contents">{weathers.data?.description}</div>
+    </div>
+  );
+};
 export default Weather;
