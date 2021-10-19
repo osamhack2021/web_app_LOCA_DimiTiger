c7ff027d0f3f9dd891f494edf88a8f37a7c6e1c9
Jay <me@wjay.kim>
2021-10-20 02:41:43 +0900

    feat(web): update team and opensource component

--- WEB(FE)/src/pages/Settings/components/OpenSource.tsx
+++ WEB(FE)/src/pages/Settings/components/OpenSource.tsx
@@ -1,5 +1,12 @@
 const Opensource = () => {
-  return <div></div>;
+  return (
+    <iframe
+      title="notice"
+      src="https://osamhack2021.github.io/web_app_LOCA_DimiTiger/licenses?tech=web"
+      width="100%"
+      height="100%"
+    />
+  );
 };
 
 export default Opensource;
