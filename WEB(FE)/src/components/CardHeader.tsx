8f7ed017e837f31f6f6c4e74fb01186fe170d18f
Jay <me@wjay.kim>
2021-10-19 12:10:41 +0900

    fix(web): apply responsive card header

--- WEB(FE)/src/components/CardHeader.tsx
+++ WEB(FE)/src/components/CardHeader.tsx
@@ -2,12 +2,16 @@ import styled from 'styled-components';
 
 const CardHeader = styled.div`
   width: 100%;
-  height: 70px;
+
+  padding: 20px 0 20px 0;
 
   display: flex;
   align-items: center;
 
   border-bottom: solid 1px #e6e6e6;
+
+  flex-wrap: wrap;
+  row-gap: 10px;
 
   padding-left: 30px;
   padding-right: 30px;
@@ -22,6 +26,11 @@ const CardHeader = styled.div`
     font-weight: 700;
     font-size: 1.5rem;
     margin-bottom: 0;
+  }
+
+  .ant-form-item,
+  .ant-btn {
+    margin-bottom: 10px;
   }
 `;
 
