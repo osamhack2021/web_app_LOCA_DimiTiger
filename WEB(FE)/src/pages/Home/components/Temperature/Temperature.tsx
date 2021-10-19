86b247cbe38650febd5aa070fd1da73adb6d13f6
Jay <me@wjay.kim>
2021-10-20 00:04:39 +0900

    feat(web): add chart expand function

--- WEB(FE)/src/pages/Home/components/Temperature/Temperature.tsx
+++ WEB(FE)/src/pages/Home/components/Temperature/Temperature.tsx
@@ -1,18 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useRecoilValue } from 'recoil';
 
 import './Temperature.css';
 
-class Temperature extends Component {
-  render() {
-    return (
-      <div id="temperature" className="dash_component">
-        <div id="temperature_head">
-          <img src="./icons/temperature.svg" alt="temperatureIco" />
-          <div id="temperature_num">31</div>
-        </div>
-        <div id="temperature_contents">온도지수 주의</div>
-      </div>
-    );
-  }
+import { useWeathers } from '@/api/weathers';
+import { settingsState } from '@/atoms';
+
+function convertValue(C: number, H: number) {
+  const F = (C * 9) / 5 + 32;
+  const R = H * 0.01;
+
+  const fahrenheit =
+    -42.379 +
+    2.04901523 * F +
+    10.14333127 * R -
+    0.22475541 * F * R -
+    0.0068377 * F * F -
+    0.05481717 * R * R +
+    0.00122874 * F * F * R +
+    0.00085282 * F * R * R -
+    0.00000199 * F;
+
+  const celcius = (fahrenheit - 32) / 1.8;
+  return Math.round(celcius * 10) / 10.0;
 }
+
+const Temperature = () => {
+  const {
+    weather: { location },
+  } = useRecoilValue(settingsState);
+  const { data } = useWeathers({ city: location });
+  return (
+    <div id="temperature" className="dash_component">
+      <div id="temperature_head">
+        <img src="./icons/temperature.svg" alt="temperatureIco" />
+        <div id="temperature_num">
+          {data && convertValue(data.temp, data.humidity)}
+        </div>
+      </div>
+      <div id="temperature_contents">온도지수 주의</div>
+    </div>
+  );
+};
 export default Temperature;
